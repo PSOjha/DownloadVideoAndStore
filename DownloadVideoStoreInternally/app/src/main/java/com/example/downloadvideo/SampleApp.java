1a4e40636ef16127c34688acf63422f351ef884a
Piyush Ojha <32029236+PSOjha@users.noreply.github.com>
2021-07-06 12:53:37 +0530

    Add files via upload

---
+++ DownloadVideoStoreInternally/app/src/main/java/com/example/downloadvideo/SampleApp.java
@@ -0,0 +1,18 @@
+package com.example.downloadvideo;
+
+import android.app.Application;
+
+import com.downloader.PRDownloader;
+import com.downloader.PRDownloaderConfig;
+
+public class SampleApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PRDownloaderConfig config = PRDownloaderConfig.newBuilder()
+                .setDatabaseEnabled(true)
+                .build();
+        PRDownloader.initialize(this, config);
+    }
+}
